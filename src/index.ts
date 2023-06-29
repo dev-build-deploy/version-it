eed51bd34ef02a023bd6c6380a9263b517594337
Kevin de Jong <monkaii@hotmail.com>
2023-06-29 09:57:48 +0200

    fix: expose ICalVer and ISemVer interfaces

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ SPDX-FileCopyrightText: 2023 Kevin de Jong <monkaii@hotmail.com>
 SPDX-License-Identifier: GPL-3.0-or-later
 */
 
-export { SemVer, SemVerIncrement } from "./semver";
-export { CalVer, CalVerIncrement } from "./calver";
+export { ISemVer, SemVer, SemVerIncrement } from "./semver";
+export { ICalVer, CalVer, CalVerIncrement } from "./calver";
 export { IVersion } from "./interfaces";
