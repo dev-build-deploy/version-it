db70cc6e119d6d966e48fd762c9160f80744e362
Kevin de Jong <monkaii@hotmail.com>
2025-04-27 08:55:17 +0200

    fix: export types correctly on package level

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@
  * SPDX-License-Identifier: MIT
  */
 
-export { ISemVer, SemVer, SemVerIncrement } from "./semver";
-export { ICalVer, CalVer, CalVerIncrement } from "./calver";
-export { Modifier } from "./modifiers";
-export { IComparable, IVersion } from "./interfaces";
+export { CalVer } from "./calver";
+export { SemVer } from "./semver";
+
+export type { ISemVer, SemVerIncrement } from "./semver";
+export type { ICalVer, CalVerIncrement } from "./calver";
+export type { Modifier } from "./modifiers";
+export type { IComparable, IVersion } from "./interfaces";
